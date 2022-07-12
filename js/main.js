5aeeb7cbb7031533a9b58d2174f06dba1c70d06e
Macy Hong <macyhong@gmail.com>
2022-07-12 03:22:28 +0000

    Listen for 'submit' events on the journal entry form. #1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
 var $photoURL = document.querySelector('#photoURL');
 var $photosrc = document.querySelector('img');
 
-$photoURL.addEventListener('change', function (event) {
+$photoURL.addEventListener('change', function changeURL(event) {
   var $photoInput = event.target.value;
   $photosrc.setAttribute('src', $photoInput);
 });
+
+// var $submit = document.querySelector('#submit');
+
+var $form = document.querySelector('form');
+$form.addEventListener('submit', logSubmit);
+
+function logSubmit(event) {
+  event.preventDefault();
+  var entry = {
+    title: $form.elements.title.value,
+    photoURL: $form.elements.photoURL.value,
+    notes: $form.elements.notes.value,
+    entryId: data.nextEntryId
+  };
+  data.nextEntryId++;
+  data.entries.unshift(entry);
+
+  $form.reset();
+  $photosrc.setAttribute('src', 'images/placeholder-image-square.jpg');
+}
