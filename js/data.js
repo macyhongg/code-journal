037993d0dd3b43004c0502923f5c904fdc5002b0
Macy Hong <macyhong@gmail.com>
2022-07-15 02:03:28 +0000

    Add an anchor to the entries view that shows the entry form. #2

--- js/data.js
+++ js/data.js
@@ -20,3 +20,16 @@ var previousEntriesJSON = localStorage.getItem('local-journal');
 if (previousEntriesJSON != null) {
   data = JSON.parse(previousEntriesJSON);
 }
+
+// Shows the entry form
+
+var $navEntries = document.querySelector('.navEntries');
+var $entryForm = document.getElementById('entry-form');
+var $entries = document.getElementById('entries');
+
+function entriesView(event) {
+  $entryForm.className = 'hidden';
+  $entries.classList.remove('hidden');
+}
+
+$navEntries.addEventListener('click', entriesView);
